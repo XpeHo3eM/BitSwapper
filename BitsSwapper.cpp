f7f9c1e3b3d6aba83133072e0e28843e609d0d04
Никулин Павел <XpeHo3eM@gmail.com>
2024-04-14 17:58:29 +0400

    refactor: changed function bitSwapper and output

--- BitsSwapper.cpp
+++ BitsSwapper.cpp
@@ -3,43 +3,55 @@
 #include <array>
 #include <cstdint>
 #include <ctime>
+#include <iomanip>
 
 
 
-void bitSwapper(uint16_t* begin, size_t byteLength) {
+void bitSwapper(uint8_t* begin, size_t byteLength) {
 	for (size_t i = 0; i < byteLength; ++i) {
 		const auto mask = rand();
 		auto& number    = begin[i];
 		
-		std::cout << std::bitset<16>(number) << '\t' << std::bitset<16>(mask) << '\t';
+		std::cout 
+			<< std::left << std::setw(10) << std::bitset<8>(number) 
+			<< std::left << std::setw(10) << std::bitset<8>(mask);
 		
 		number ^= mask;
 
-		std::cout << std::bitset<16>(number) << std::endl;
+		std::cout 
+			<< std::left << std::setw(10) << std::bitset<8>(number) 
+			<< std::endl;
 	}
 }
 
+
+void print(uint8_t* arr, size_t size) {
+	for (size_t i = 0; i < size; ++i) {
+		std::cout << static_cast<unsigned int>(arr[i]) << ' ';
+	}
+}
 
 
 int main() {
 	srand(time(0));
 	
-	std::array<uint16_t, 10> arr;
+	std::array<uint8_t, 10> arr;
 	
 	for (auto& el : arr) {
 		el = rand();
 	}
 
 	std::cout << "Before swapper: " << std::endl;
-	for (const auto& el : arr) {
-		std::cout << el << ' ';
-	}
+	print(arr.data(), arr.size());
 	std::cout << "\n\n\n";
 	
+	std::cout 
+		<< std::left << std::setw(10) << "Number" 
+		<< std::left << std::setw(10) << "Mask" 
+		<< std::left << std::setw(10) << "Swapped" 
+		<< std::endl;
 	bitSwapper(arr.data(), arr.size());
 
 	std::cout << "\n\nAfter swapper: " << std::endl;
-	for (const auto& el : arr) {
-		std::cout << el << ' ';
-	}
+	print(arr.data(), arr.size());
 }
