337c8437785ff38af80aad6781dbfeaa52398e5c
Никулин Павел <npv90@mail.ru>
2024-04-15 09:08:34 +0400

    refactor: Updated input data type by template

--- BitsSwapper.cpp
+++ BitsSwapper.cpp
@@ -7,11 +7,12 @@
 
 
 
-void bitSwapper(uint8_t* begin, size_t byteLength) {
+template<class T>
+void bitSwapper(T begin, size_t byteLength) {
 	for (size_t i = 0; i < byteLength; ++i) {
 		const auto mask = rand();
-		auto& number    = begin[i];
-		
+		auto& number    = *(reinterpret_cast<uint8_t*>(begin) + i);
+
 		std::cout 
 			<< std::left << std::setw(10) << std::bitset<8>(number) 
 			<< std::left << std::setw(10) << std::bitset<8>(mask);
@@ -25,9 +26,12 @@ void bitSwapper(uint8_t* begin, size_t byteLength) {
 }
 
 
-void print(uint8_t* arr, size_t size) {
+template<class T>
+void print(T arr, size_t size) {
+	bool uint8t = std::is_same<T, uint8_t*>::value;
+
 	for (size_t i = 0; i < size; ++i) {
-		std::cout << static_cast<unsigned int>(arr[i]) << ' ';
+		std::cout << (uint8t ? static_cast<unsigned int>(arr[i]) : arr[i]) << ' ';
 	}
 }
 
@@ -35,7 +39,7 @@ void print(uint8_t* arr, size_t size) {
 int main() {
 	srand(time(0));
 	
-	std::array<uint8_t, 10> arr;
+	std::array<uint16_t, 10> arr;
 	
 	for (auto& el : arr) {
 		el = rand();
@@ -50,7 +54,7 @@ int main() {
 		<< std::left << std::setw(10) << "Mask" 
 		<< std::left << std::setw(10) << "Swapped" 
 		<< std::endl;
-	bitSwapper(arr.data(), arr.size());
+	bitSwapper(arr.data(), 7);
 
 	std::cout << "\n\nAfter swapper: " << std::endl;
 	print(arr.data(), arr.size());
